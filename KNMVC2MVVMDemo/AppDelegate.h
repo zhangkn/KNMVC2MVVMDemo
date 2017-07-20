b4acef8a64ded2ae876de00170a1b99b87056bf2
kevin <929118967@qq.com>
2017-07-20 17:33:38 +0800

    Initial Commit

---
+++ KNMVC2MVVMDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KNMVC2MVVMDemo
+//
+//  Created by devzkn on 20/07/2017.
+//  Copyright © 2017 hisun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KNMVC2MVVMDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KNMVC2MVVMDemo
+//
+//  Created by devzkn on 20/07/2017.
+//  Copyright © 2017 hisun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
